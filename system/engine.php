7473c5f7bd7032264dfa9e28288bc22f521e6d3e
Krzysztof Golinski <kgolinski@pangenerator.com>
2012-09-11 23:41:34 +0200

    dropped klieg - routing not really needed

--- system/engine.php
+++ system/engine.php
@@ -4,47 +4,27 @@ require 'lib/Mustache/Autoloader.php';
 include_once "lib/markdown.php";
 include('lib/spyc.php');
 include('functions.php');
-require 'lib/klein.php';
 Mustache_Autoloader::register();
 $arr = getDirectory(CONTENT);
 ksort($arr);
+$mustache = new Mustache_Engine(array('loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/../'.TEMPLATE)));
+$pageTpl = $mustache->loadTemplate('page');
+$nav = array();
+//menu construction
+echo arr2nav($arr);
+//content selection:
+reset($arr);
+$key = key($arr);
+$post_title = $arr[$key]['title'];
 
-respond(function ($request, $response) { //probably not the best way to do this:
-	$mustache = new Mustache_Engine(array('loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/../'.TEMPLATE)));    
-	$response->renderTemplate = function ($tmpl, $content) use ($mustache) {
-		$template = $mustache->loadTemplate($tmpl);
-        echo $template->render($content);
-    };
-});
+$pageVars['title'] = $post_title;
+$pageVars['page_title'] = TITLE . ' ' . $post_title;
+$pageVars['content'] = $arr[$key]['content'];
 
-//HOME - first element in the array
-respond(BASE."/",function ($request, $response) use ($arr){
-	reset($arr);
-	$key = key($arr);
-	$post_title = $arr[$key]['title'];
-	$pageVars = array();
-	$pageVars['title'] = $post_title;
-	$pageVars['page_title'] = TITLE . ' ' . $post_title;
-	$pageVars['content'] = $arr[$key]["content"];	
-	$response->renderTemplate('page', $pageVars);
-});
+//subpages
+//404
 
-/*
-respond(BASE.'/[:name]', function ($request) {
-	$post_title = 'Post Title';
-	$pageVars = array();
-	$pageVars['title'] = $post_title;
-	$pageVars['page_title'] = TITLE . ' ' . $post_title;
-	$page['nav'] = array('one','two','three');
-	$pageVars['content'] = 'content';
-	
-	$response->renderTemplate($pageTpl, $pageVars);
-});
-*/
-//home:
-
-
-dispatch();
+echo $pageTpl->render($pageVars);
 
 //maybe for later:?
 //$allTags = array_unique($allTags);
@@ -60,5 +40,18 @@ $page['posts'].=$postTpl->render($post);
 
 //echo $pageTpl->render($pageVars);
 
-//respond('/klein/posts', function() { echo 'Wszyscy umrzemy'; });
+function arr2nav($array) {
+    $out='<ul>'."\n";
+    foreach($array as $key => $elem){
+        if(!is_array($elem)){
+          //$out=$out.'<li>'. $elem['title'].'</li>'."\n";
+        }
+        else $out=$out.'<li>'.$elem['title'];
+		//if()
+			//$out = $out . arr2nav($elem).'</li>'."\n";
+		$out = $out . '</li>'."\n";
+    }
+    $out=$out.'</ul>'."\n";
+    return $out; 
+}
 ?>
